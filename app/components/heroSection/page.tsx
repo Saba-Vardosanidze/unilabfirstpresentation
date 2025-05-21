2cbed04612356b5abc9d367fb43eb3788585440c
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-05-22 02:33:01 +0400

    update

--- app/components/heroSection/page.tsx
+++ app/components/heroSection/page.tsx
@@ -18,7 +18,9 @@ const HeroSection = () => {
           </div>
           <div className="relative w-full max-w-[600px] aspect-square">
             <div className="flex justify-center items-center border border-gray-600 rounded-full w-full h-full">
-              <img src="/public/vectors/logo.svg" alt="" />
+              <div className="shadow-[0px_0px_188px_40px_rgba(85,71,235,0.5)] rounded-full">
+                <img className="mt-[100px]" src="/vectors/Logo.svg" alt="" />
+              </div>
             </div>
             <motion.div
               className="top-0 right-0 bottom-0 left-0 absolute"
