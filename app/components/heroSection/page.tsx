6cacd6809fb4b65b3cbf7220227111e4b8fb8d54
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-05-21 22:55:47 +0400

    update

---
+++ app/components/heroSection/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import {motion} from "framer-motion";
+
+const HeroSection = () => {
+  return (
+    <div className="bottom-1/2 absolute flex justify-center items-center m-auto w-full">
+      <div className="relative flex justify-center items-center border border-gray-600 rounded-full w-full max-w-[900px] aspect-square">
+        <div className="absolute rounded-full w-full h-full">
+          <motion.div
+            className="top-0 right-0 bottom-0 left-0 absolute"
+            animate={{rotate: 360}}
+            transition={{repeat: Infinity, duration: 20, ease: "linear"}}
+          >
+            <div className="bottom-[-25px] left-1/2 absolute bg-[#171717] p-[3px] border border-gray-600 rounded-full w-[50px] h-[50px] overflow-hidden"></div>
+            <div className="top-[-25px] left-1/2 absolute bg-[#171717] p-[3px] border border-gray-600 rounded-full w-[50px] h-[50px] overflow-hidden"></div>
+          </motion.div>
+        </div>
+        <div className="relative w-full max-w-[600px] aspect-square">
+          <div className="border border-gray-600 rounded-full w-full h-full" />
+          <motion.div
+            className="top-0 right-0 bottom-0 left-0 absolute"
+            animate={{rotate: 360}}
+            transition={{repeat: Infinity, duration: 20, ease: "linear"}}
+          >
+            <div className="bottom-[-25px] left-1/2 absolute bg-[#171717] p-[3px] border border-gray-600 rounded-full w-[50px] h-[50px] overflow-hidden"></div>
+            <div className="top-[-25px] left-1/2 absolute bg-[#171717] p-[3px] border border-gray-600 rounded-full w-[50px] h-[50px] overflow-hidden"></div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
