d50892b66aa8a993cf447b85c39b0ada2b05b75d
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-05-22 13:21:37 +0400

    update

--- app/components/heroSection/page.tsx
+++ app/components/heroSection/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import {motion} from "framer-motion";
+import {Earth, Mars, Mercury, Sun, Venus} from "@/app/features/images/svg";
 
 const HeroSection = () => {
   return (
@@ -12,14 +13,18 @@ const HeroSection = () => {
               animate={{rotate: 360}}
               transition={{repeat: Infinity, duration: 20, ease: "linear"}}
             >
-              <div className="bottom-[-25px] left-1/2 absolute bg-[#05051A] p-[3px] border border-gray-600 rounded-full w-[50px] h-[50px] overflow-hidden"></div>
-              <div className="top-[-25px] left-1/2 absolute bg-[#05051A] p-[3px] border border-gray-600 rounded-full w-[50px] h-[50px] overflow-hidden"></div>
+              <div className="bottom-[-35px] left-1/2 absolute flex justify-center items-center bg-[#05051A] p-[3px] rounded-full w-[70px] h-[70px] overflow-hidden">
+                <Venus width={100} height={100} />
+              </div>
+              <div className="top-[-35px] left-1/2 absolute flex justify-center items-center bg-[#05051A] p-[3px] rounded-full w-[70px] h-[70px] overflow-hidden">
+                <Earth width={100} height={100} />
+              </div>
             </motion.div>
           </div>
           <div className="relative w-full max-w-[600px] aspect-square">
             <div className="flex justify-center items-center border border-gray-600 rounded-full w-full h-full">
-              <div className="shadow-[0px_0px_188px_40px_rgba(85,71,235,0.5)] rounded-full">
-                <img className="mt-[100px]" src="/vectors/Logo.svg" alt="" />
+              <div className="shadow-[0px_0px_250px_100px_rgba(255,240,180,0.01)] mt-[100px] rounded-full">
+                <Sun width={200} height={200} />
               </div>
             </div>
             <motion.div
@@ -27,8 +32,12 @@ const HeroSection = () => {
               animate={{rotate: 360}}
               transition={{repeat: Infinity, duration: 30, ease: "linear"}}
             >
-              <div className="bottom-[-25px] left-1/2 absolute bg-[#05051A] p-[3px] border border-gray-600 rounded-full w-[50px] h-[50px] overflow-hidden"></div>
-              <div className="top-[-25px] left-1/2 absolute bg-[#05051A] p-[3px] border border-gray-600 rounded-full w-[50px] h-[50px] overflow-hidden"></div>
+              <div className="bottom-[-25px] left-1/2 absolute flex justify-center items-center bg-[#05051A] p-[3px] rounded-full w-[50px] h-[50px] overflow-hidden">
+                <Mars width={50} height={50} />
+              </div>
+              <div className="top-[-25px] left-1/2 absolute flex justify-center items-center bg-[#05051A] p-[3px] rounded-full w-[50px] h-[50px] overflow-hidden">
+                <Mercury width={100} height={100} />
+              </div>
             </motion.div>
           </div>
         </div>
---
+++ app/features/images/svg/index.ts
@@ -0,0 +1,5 @@
+export {default as Earth} from "./earth.svg";
+export {default as Mars} from "./mars.svg";
+export {default as Mercury} from "./Mercury.svg";
+export {default as Sun} from "./Sun.svg";
+export {default as Venus} from "./Venus.svg";
