8fcf7e534e1fe36c66a8b097dbc0b236ddb156db
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-05-22 14:34:18 +0400

    update

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-
+import Image from "next/image";
 const Header = () => {
   return (
-    <div className="bg-[#050523] border-gray-600 border-b w-full min-h-[72px]"></div>
+    <header className="flex justify-center items-center bg-[#050523] px-[35px] border-gray-600 border-b w-full min-h-[89px]">
+      <div className="flex justify-between w-full max-w-[1440px]">
+        <div className="flex gap-[87px]">
+          <Image
+            className="cursor-pointer"
+            src="/vectors/Logo.svg"
+            alt="siteLogo"
+            width={50}
+            height={50}
+          />
+          <ul className="hidden lg:flex items-center gap-[40px] text-white">
+            <li>Mercury</li>
+            <li>Venus</li>
+            <li>Earth</li>
+            <li>Mars</li>
+            <li>Jupiter</li>
+          </ul>
+        </div>
+        <div className="hidden lg:flex items-center gap-[35px] text-[12px] text-white">
+          <button className="flex justify-center items-center px-[32px] py-[8px] cursor-pointer">
+            Request A Demo
+          </button>
+          <button className="flex justify-center items-center px-[25px] py-[8px] cursor-pointer">
+            Start your free trial
+          </button>
+        </div>
+      </div>
+    </header>
   );
 };
 
--- app/components/heroSection/page.tsx
+++ app/components/heroSection/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {motion} from "framer-motion";
 import {Earth, Mars, Mercury, Sun, Venus} from "@/app/features/images/svg";
+import {Repeat} from "lucide-react";
 
 const HeroSection = () => {
   return (
@@ -13,30 +14,34 @@ const HeroSection = () => {
               animate={{rotate: 360}}
               transition={{repeat: Infinity, duration: 20, ease: "linear"}}
             >
-              <div className="bottom-[-35px] left-1/2 absolute flex justify-center items-center bg-[#05051A] p-[3px] rounded-full w-[70px] h-[70px] overflow-hidden">
+              <div className="bottom-[-35px] left-1/2 absolute flex justify-center items-center bg-[#05051A] rounded-full w-[70px] h-[70px] overflow-hidden">
                 <Venus width={100} height={100} />
               </div>
-              <div className="top-[-35px] left-1/2 absolute flex justify-center items-center bg-[#05051A] p-[3px] rounded-full w-[70px] h-[70px] overflow-hidden">
+              <div className="top-[-35px] left-1/2 absolute flex justify-center items-center bg-[#05051A] rounded-full w-[70px] h-[70px] overflow-hidden">
                 <Earth width={100} height={100} />
               </div>
             </motion.div>
           </div>
           <div className="relative w-full max-w-[600px] aspect-square">
             <div className="flex justify-center items-center border border-gray-600 rounded-full w-full h-full">
-              <div className="shadow-[0px_0px_250px_100px_rgba(255,240,180,0.01)] mt-[100px] rounded-full">
+              <motion.div
+                className="shadow-[0px_0px_250px_100px_rgba(255,240,180,0.01)] mt-[100px] rounded-full"
+                animate={{rotate: 360}}
+                transition={{repeat: Infinity, duration: 20, ease: "linear"}}
+              >
                 <Sun width={200} height={200} />
-              </div>
+              </motion.div>
             </div>
             <motion.div
               className="top-0 right-0 bottom-0 left-0 absolute"
               animate={{rotate: 360}}
               transition={{repeat: Infinity, duration: 30, ease: "linear"}}
             >
-              <div className="bottom-[-25px] left-1/2 absolute flex justify-center items-center bg-[#05051A] p-[3px] rounded-full w-[50px] h-[50px] overflow-hidden">
+              <div className="bottom-[-35px] left-1/2 absolute flex justify-center items-center bg-[#05051A] rounded-full w-[70px] h-[70px] overflow-hidden">
                 <Mars width={50} height={50} />
               </div>
-              <div className="top-[-25px] left-1/2 absolute flex justify-center items-center bg-[#05051A] p-[3px] rounded-full w-[50px] h-[50px] overflow-hidden">
-                <Mercury width={100} height={100} />
+              <div className="top-[-35px] left-1/2 absolute flex justify-center items-center bg-[#05051A] rounded-full w-[70px] h-[70px] overflow-hidden">
+                <Mercury width={120} height={120} />
               </div>
             </motion.div>
           </div>
